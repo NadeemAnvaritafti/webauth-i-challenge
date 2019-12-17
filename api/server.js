896533e8782c062a5a42a60d48bc11711b81d934
Nadeem Anvaritafti <nanvari@live.com>
2019-12-17 14:21:06 -0600

    downloaded dependencies and added session configuration in server.js

--- api/server.js
+++ api/server.js
@@ -1,17 +1,48 @@
+// ---- imports 
 const express = require('express');
 const helmet = require('helmet');
+const sessions = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(sessions);
 
 const authRouter = require('../auth/auth-router');
 const usersRouter = require('../users/users-router');
 
+const knex = require('../data/db-config');
+
+
+// ---- calling express as variable 
 const server = express();
 
+
+// ---- session configuration 
+const sessionConfig = {
+    name: 'validatedUser',
+    secret: 'super encrypted secret code',
+    saveUninitialized: true,
+    resave: false,
+
+    store: new KnexSessionStore({
+        knex,
+        createtable: true,
+        clearInterval: 1000 * 60 * 10,
+        sidfieldname: 'sid',
+        tablename: 'sessions'
+    }),
+
+    cookie: {
+        maxAge: 1000 * 60 * 10,
+        secure: false,
+        httpOnly: true
+    }
+};
+
+// ---- using/calling the imports
 server.use(helmet());
 server.use(express.json());
+server.use(sessions(sessionConfig));
+
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
-
-
 
 
 module.exports = server;
